b7420fe6c983e60287ecd6401acad1b086293f1a
sspl-schaturvedi <schaturvedi@stepoutsolutions.com>
2015-01-15 15:03:32 +0530

    Removed non-required code

--- bueno-m/front-page.php
+++ bueno-m/front-page.php
@@ -8,21 +8,9 @@
 
 <header id="top" class="header">
   <div class="text-vertical-center"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/splash-01.png" class="img-responsive" style="margin:100px auto auto auto;" /> 
-    <!--div class="splash-logo"><img src="kitchen/images/splash-logo.png" /></div>
-    <h1 class="splash-title">Goodness</h1>
-    <div class="splash-section">
-      <div class="lato-regular desktop-only" id="splash-taglines-container">
-        <h5>Global Cuisine</h5>
-        <h5>Delivery and Service</h5>
-        <h5>Catering For All Occasions</h5>
-      </div>
-      <div class="splash-tagline desktop-only"><img src="kitchen/images/inside-trail.png" /></div>
-      <div class="splash-tagline mobile-only"><img src="kitchen/images/inside.png" /></div>
-    </div--> 
     <br clear="all">
     <div class="splash-section" style="margin-bottom:10px;"> <a href="home" class="btn btn-light btn-splash">Explore<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-splash-proceed.png"></a> </div>
     <div class="splash-section"> <a href="order" class="btn btn-light btn-splash">Order<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-splash-proceed.png"></a> </div>
-    <!--div class="splash-section mobile-only"> <a href="order/order-static" class="btn btn-light btn-splash">Order.<img src="<?php //echo get_stylesheet_directory_uri(); ?>/images/icon-splash-proceed.png"></a> </div-->
   </div>
   </div>
 </header>
