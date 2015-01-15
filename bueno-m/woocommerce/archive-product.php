1d88b43fe5ab8ce1e77534e61651ff8366f88a57
Aditya Goyal <agoyal@stepoutsolutions.com>
2015-01-15 18:30:32 +0530

    Removed Order page default title

--- bueno-m/woocommerce/archive-product.php
+++ bueno-m/woocommerce/archive-product.php
@@ -25,8 +25,6 @@ get_header( 'shop' ); ?>
 
 		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
-			<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
-
 		<?php endif; ?>
 
 		<?php do_action( 'woocommerce_archive_description' ); ?>
