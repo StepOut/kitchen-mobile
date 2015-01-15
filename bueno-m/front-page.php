2a859a9ea3760e963572a3ee2967978bce2bf0c5
sspl-schaturvedi <schaturvedi@stepoutsolutions.com>
2015-01-15 15:51:57 +0530

    Updated css for splash

--- bueno-m/front-page.php
+++ bueno-m/front-page.php
@@ -9,8 +9,8 @@
 <header id="top" class="header">
   <div class="text-vertical-center"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/splash-01.png" class="img-responsive" style="margin:100px auto auto auto;" /> 
     <br clear="all">
-    <div class="splash-section" style="margin-bottom:10px;"> <a href="home" class="btn btn-light btn-splash">Explore<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-splash-proceed.png"></a> </div>
-    <div class="splash-section"> <a href="order" class="btn btn-light btn-splash">Order<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-splash-proceed.png"></a> </div>
+    <div class="splash-section" style="margin-bottom:10px;"> <a href="m/home" class="btn btn-success btn-splash">Explore<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-splash-proceed.png"></a> </div>
+    <div class="splash-section"> <a href="order" class="btn btn-success btn-splash">Order<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-splash-proceed.png"></a> </div>
   </div>
   </div>
 </header>
---
+++ outside theme/dev-backups/front-page.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Bueno</title>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <link rel="shortcut icon" href="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/flat-ui/images/favicon.ico">
+        <link rel="stylesheet" href="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/flat-ui/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/flat-ui/css/flat-ui.css">
+        <!-- Using only with Flat-UI (free)-->
+        <link rel="stylesheet" href="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/common-files/css/icon-font.css">
+        <!-- end -->
+        <link rel="stylesheet" href="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/css/style.css">
+    </head>
+
+    <body>
+
+        <div class="page-wrapper">
+            <!-- header-4 -->
+            <header class="header-4 v-center bg-midnight-blue">
+                <div class="background">
+                    &nbsp;
+                </div>
+                <div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="hero-unit">
+                                	<img src="https://dl.dropboxusercontent.com/u/36260810/running-files/bueno/round-unit.png">
+                                    <h1>Authentic Global Food.</h1>
+                                    <p>
+                                        Served &amp; Delivered
+                                    </p>
+                                </div>
+                                <div class="btns">
+                                    <a class="btn btn-large btn-primary" href="http://bueno.co.in/v2/m-start/">Place Order</a>
+                                    <a class="btn btn-large btn-gray" href="http://bueno.co.in/v2/my-account/">Login / Register</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/common-files/js/jquery-1.10.2.min.js"></script>
+        <script src="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/flat-ui/js/bootstrap.min.js"></script>
+        <script src="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/common-files/js/modernizr.custom.js"></script>
+        <script src="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/common-files/js/jquery.parallax.min.js"></script>
+        <script src="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/common-files/js/startup-kit.js"></script>
+        <script src="http://bueno.co.in/v2/wp-content/themes/mobili/homepage-ui/js/script.js"></script>
+    </body>
+</html>
