45e729fec9523270e7c0d23965ebd3b8d9702fef
Aditya Goyal <agoyal@stepoutsolutions.com>
2015-01-16 13:27:45 +0530

    Added script

--- bueno-m/js/bueno-m-scripts.js
+++ bueno-m/js/bueno-m-scripts.js
@@ -4,3 +4,16 @@ jQuery(document).on("click", ".add_quantity", function(){
 	jQuery(this).hide();
 });
 
+jQuery(document).on("click", "#add-service label.service", function() { //saves selected service in session
+	jQuery(this).children(".select-service").toggle();
+	jQuery(this).children(".unselect-service").toggle();
+    jQuery.ajax({
+		type: 'POST',
+    	url: '../save-comment',
+    	data: {
+			service: jQuery(this).children("input[type=hidden]").val()
+		},
+       	success: function(response, textStatus, jqXHR){
+        }
+	});
+});
