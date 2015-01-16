3c0128afbbc161031fdcfb8bd7554d6c85e2e832
Aditya Goyal <agoyal@stepoutsolutions.com>
2015-01-16 13:21:00 +0530

    Added custom script

--- bueno-m/footer.php
+++ bueno-m/footer.php
@@ -3,5 +3,7 @@
 		</div>
         <!-- /Content -->
 		<?php wp_footer(); ?>
+        
+        <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/bueno-m-scripts.js"></script>
 	</body>
 </html>
